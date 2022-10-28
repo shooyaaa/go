6ba6027151a01f75fad3d868e3b8b992941726d1
luanxun <xun.luan@funplus.com>
2022-10-28 09:28:16 +0800

    modify mouse client

--- runnable/unimouse/client.go
+++ runnable/unimouse/client.go
@@ -2,7 +2,11 @@ package unimouse
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
 
+	"github.com/shooyaaa/core/codec"
 	. "github.com/shooyaaa/core/network"
 	"github.com/shooyaaa/core/session"
 	"github.com/shooyaaa/log"
@@ -10,13 +14,17 @@ import (
 
 func Connect() {
 	tcp := TcpConn{}
-	session, err := tcp.Dial("localhost", 9994)
+	s, err := tcp.Dial("10.1.36.39", 9994)
 	if err != nil {
 		log.Fatal("error occurs while connect to server %v", err)
 	}
-
+	session.SetCodec(&codec.Json{})
 	handler := Handler{}
-	session.SetOwner(handler)
+	s.SetOwner(handler)
+	ch := make(chan os.Signal, 1)
+	log.Info(http.ListenAndServe("localhost:6060", nil).Error())
+	signal.Notify(ch, os.Interrupt, os.Kill)
+	<-ch
 }
 
 type ClientHandler struct {
